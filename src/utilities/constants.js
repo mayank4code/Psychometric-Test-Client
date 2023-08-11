8cb69f312be865d4a42329d7d1d31f5dbe2ad1e5
mayank4code <119710987+mayank4code@users.noreply.github.com>
2023-08-11 23:03:33 +0530

    deployed server linked

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,5 +1,7 @@
 // const server_origin = "https://psychometric-test-api.onrender.com";
-const server_origin = 'http://localhost:5000';
+
+const server_origin = "https://psychometric-test-server.onrender.com";
+// const server_origin = 'http://localhost:5000';
 
 
 export {server_origin};
